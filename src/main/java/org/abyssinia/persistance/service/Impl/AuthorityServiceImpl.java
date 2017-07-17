f3d342765315399e0e6c908135a0d6181b7171af
Gobi <yaphet.gobenak@gmail.com>
2017-07-17 16:03:47 -0500

    Pom editted, added new packages

---
+++ src/main/java/org/abyssinia/persistance/service/Impl/AuthorityServiceImpl.java
@@ -0,0 +1,40 @@
+package org.abyssinia.persistance.service.Impl;
+
+import java.util.List;
+
+import org.abyssinia.persistance.service.AuthorityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import edu.mum.domain.Authority;
+
+@Service
+public class AuthorityServiceImpl implements AuthorityService{
+
+	@Autowired
+	
+	
+	@Override
+	public void save(Authority authority) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Authority authority) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Authority> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Authority findOne(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
